90393223f5d593850638f81aaf47afaa22d4df14
Chi Señires <chi.senires@gmail.com>
2018-02-01 20:13:58 +0800

    Setting up full screen slider for home page
    Trying out various JS sliders to see which one works best

---
+++ js/main.js
@@ -0,0 +1,12 @@
+$(document).ready(function() {
+	// $('#above-fold-slider').lightSlider({
+	// 	item: 1,
+	// 	auto: true,
+	// 	loop: true,
+	// 	pause: 3000,
+	// 	speed: 800,
+	// 	slideMargin: 0,
+	// 	autoWidth: true,
+	// });
+	$('#slides').superslides('start');
+});
